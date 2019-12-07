a216e9c499951df31a652458a091cfbddc1a4619
Bernd Olleck <Bernd.Olleck@it-beratung.olleck.de>
2019-12-07 19:55:06 +0100

    more examples

--- src/app/training-examples/training-examples.component.ts
+++ src/app/training-examples/training-examples.component.ts
@@ -1,74 +1,198 @@
-import {Component, OnInit} from '@angular/core';
-import {asapScheduler, asyncScheduler, merge, of, queueScheduler, Subject} from 'rxjs';
-import {filter, observeOn, startWith} from 'rxjs/operators';
+import {Component} from '@angular/core';
+import {
+  asapScheduler,
+  asyncScheduler,
+  combineLatest,
+  concat,
+  ConnectableObservable, forkJoin,
+  fromEvent,
+  interval,
+  merge,
+  of,
+  queueScheduler, race,
+  ReplaySubject,
+  Subject, zip
+} from 'rxjs';
+import {filter, map, observeOn, publish, share, startWith, take, takeUntil} from 'rxjs/operators';
 
 @Component({
   selector: 'app-training-examples',
   templateUrl: './training-examples.component.html',
   styleUrls: ['./training-examples.component.css']
 })
-export class TrainingExamplesComponent implements OnInit {
+export class TrainingExamplesComponent {
 
   title = 'InitialSet';
+  logStartTime = new Date().getTime();
 
-  ngOnInit() {
+  stop$ = new Subject();
+
+  stop() {
+    this.stop$.next();
+  }
+
+  resetLogTime() {
+    this.logStartTime = new Date().getTime();
+  }
+
+  log(...args) {
+    console.log(new Date().getTime() - this.logStartTime, ...args);
   }
 
   startNoScheduler() {
-    const subject = new Subject<boolean>();
-    const obs = subject.asObservable();
+    this.resetLogTime();
+    const subject$ = new Subject<boolean>();
+    const obs$ = subject$.asObservable();
 
-    obs.subscribe(v => console.log('next value', v));
+    obs$.subscribe(v => this.log('next value', v));
 
-    console.log('Going to send next value');
+    this.log('Going to send next value');
 
-    subject.next(true);
+    subject$.next(true);
 
-    console.log('Sent next value');
+    this.log('Sent next value');
   }
 
   startAsyncScheduler() {
-    const subject = new Subject<boolean>();
+    this.resetLogTime();
+    const subject$ = new Subject<boolean>();
 
-    const obs = subject.asObservable();
-    obs.pipe(observeOn(asyncScheduler))
-      .subscribe(v => console.log('next value', v));
+    const obs$ = subject$.asObservable();
+    obs$.pipe(observeOn(asyncScheduler))
+      .subscribe(v => this.log('next value', v));
 
-    console.log('Going to send next value');
+    this.log('Going to send next value');
 
-    subject.next(true);
+    subject$.next(true);
 
-    console.log('Sent next value');
+    this.log('Sent next value');
   }
 
   startMicrotaskVsPromise() {
-    queueMicrotask(() => console.log('queueMicrotask 1'));
-    Promise.resolve().then(() => console.log('promise 1'));
-    Promise.resolve().then(() => console.log('promise 2'));
-    queueMicrotask(() => console.log('queueMicrotask 2'));
-    console.log('Scheduling done!');
+    this.resetLogTime();
+    queueMicrotask(() => this.log('queueMicrotask 1'));
+    Promise.resolve().then(() => this.log('promise 1'));
+    Promise.resolve().then(() => this.log('promise 2'));
+    queueMicrotask(() => this.log('queueMicrotask 2'));
+    this.log('Scheduling done!');
   }
 
   startSchedulerComparison() {
-    setTimeout(() => console.log('Now Macrotasks are been processed:'));
-    Promise.resolve().then(() => console.log('Now Microtasks are being processed:'));
+    this.resetLogTime();
+    setTimeout(() => this.log('Now Macrotasks are been processed:'));
+    Promise.resolve().then(() => this.log('Now Microtasks are being processed:'));
 
-    const asyncObs = of('').pipe(
+    const async$ = of('').pipe(
       startWith('asyncScheduler', asyncScheduler));
 
-    const asapObs = of('').pipe(
+    const asap$ = of('').pipe(
       startWith('asapScheduler', asapScheduler));
 
-    const queueObs = of('').pipe(
+    const queue$ = of('').pipe(
       startWith('queueScheduler', queueScheduler));
 
     merge(
-      asyncObs,
-      asapObs,
-      queueObs).pipe(
+      async$,
+      asap$,
+      queue$).pipe(
       filter(x => !!x)
-    ).subscribe(console.log);
+    ).subscribe(this.log);
 
-    console.log('after subscription');
+    this.log('after subscription');
   }
+
+  startColdObservable() {
+    this.resetLogTime();
+    const cold$ = interval(1000);
+    cold$.pipe(take(10)).subscribe((i) => this.log('#1:', i));
+    setTimeout(
+      () => cold$.pipe(take(10)).subscribe((i) => this.log('#2:', i))
+      , 4000);
+  }
+
+  startHotObservable() {
+    this.resetLogTime();
+    const hot$ = fromEvent(document, 'click').pipe(
+      map((clickEvent: MouseEvent) => ({x: clickEvent.clientX, y: clickEvent.clientY}))
+    );
+    hot$.pipe(take(5)).subscribe((pos) => this.log('#1:', pos));
+    setTimeout(
+      () => hot$.pipe(take(5)).subscribe((pos) => this.log('#2:', pos))
+      , 4000);
+  }
+
+  startConvertHot1Observable() {
+    this.resetLogTime();
+    const cold$ = interval(1000);
+    const hot$ = cold$.pipe(publish()) as ConnectableObservable<number>;
+    hot$.connect();
+
+    setTimeout(
+      () => hot$.pipe(take(10)).subscribe((i) => this.log('#1:', i)),
+      2000);
+    setTimeout(
+      () => hot$.pipe(take(10)).subscribe((i) => this.log('#2:', i)),
+      4000);
+  }
+
+  startConvertHot2Observable() {
+    this.resetLogTime();
+    const cold$ = interval(1000);
+    const hot$ = new Subject();
+    cold$.subscribe(hot$);
+    hot$.pipe(take(10)).subscribe((i) => this.log('#1:', i));
+    setTimeout(
+      () => hot$.pipe(take(10)).subscribe((i) => this.log('#2:', i))
+      , 4000);
+  }
+
+  startConvertColdObservable() {
+    this.resetLogTime();
+    const hot$ = fromEvent(document, 'click').pipe(
+      map((clickEvent: MouseEvent) => ({x: clickEvent.clientX, y: clickEvent.clientY}))
+    );
+    const cold$ = new ReplaySubject();
+    hot$.subscribe(cold$);
+    cold$.pipe(take(5)).subscribe((pos) => this.log('#1:', pos));
+    setTimeout(
+      () => cold$.pipe(take(5)).subscribe((pos) => this.log('#2:', pos))
+      , 4000);
+  }
+
+  startWarmObservable() {
+    this.resetLogTime();
+    const warm$ = interval(1000).pipe(share());
+    setTimeout(
+      () => warm$.pipe(take(10)).subscribe((i) => this.log('#1:', i)),
+      2000);
+    setTimeout(
+      () => warm$.pipe(take(10)).subscribe((i) => this.log('#2:', i)),
+      4000);
+  }
+
+  startCombinedCreation() {
+    this.resetLogTime();
+    const values$ = of('A', 'B', 'C', 'D', 'E');
+    const interval$ = interval(1000).pipe(takeUntil(this.stop$));
+    const first$ = values$;
+    const second$ = interval$;
+    const combineLatest$ = combineLatest(first$, second$);
+    const forkJoin$ = forkJoin(first$, second$);
+    const concat$ = concat(first$, second$);
+    const merge$ = merge(first$, second$);
+    const race$ = race<any>(first$, second$);
+    const zip$ = zip(first$, second$);
+
+    combineLatest$.subscribe((v) => this.log('combineLatest:', v));
+    concat$.subscribe((v) => this.log('concat:', v));
+    forkJoin$.subscribe((v) => this.log('forkJoin:', v));
+    merge$.subscribe((v) => this.log('merge:', v));
+    race$.subscribe((v) => this.log('race:', v));
+    zip$.subscribe((v) => this.log('zip:', v));
+  }
+
+  startCombinedHigherOrder() {
+
+  }
+
 }
